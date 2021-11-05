0f6677871103e29c815b706a1ce7304fa34b0d47
Aysel ipek <aysel.ipek@bil.omu.edu.tr>
2021-11-05 19:28:15 +0300

    added dashboard statistics

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 
 namespace CoreDemo.Controllers
@@ -12,7 +13,10 @@ namespace CoreDemo.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
-
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x => x.WriterID == 1).Count();
+            ViewBag.V3 = c.Categories.Count();
             return View();
         }
     }
