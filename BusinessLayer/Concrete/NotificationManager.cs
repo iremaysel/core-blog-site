1c5337da70ec94af91408af552b702e65492ce77
Aysel ipek <aysel.ipek@bil.omu.edu.tr>
2021-11-15 17:52:26 +0300

    add Notification Class

---
+++ BusinessLayer/Concrete/NotificationManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class NotificationManager : INotificationService
+    {
+        INotificationDal _notificationDal;
+
+        public NotificationManager(INotificationDal notificationDal)
+        {
+            _notificationDal = notificationDal;
+        }
+
+        public void TAdd(Notification t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Notification t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Notification t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Notification> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Notification TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -33,5 +33,7 @@ namespace DataAccessLayer.Concrete
         public DbSet<NewsLetter> NewsLetters { get; set; }
 
         public DbSet<BlogRayting> BlogRaytings { get; set; }
+
+        public DbSet<Notification> Notifications { get; set; }
     }
 }
