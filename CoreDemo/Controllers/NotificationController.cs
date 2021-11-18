76b2466ee926a081b1a144b3372bbd999bddaedb
Aysel ipek <aysel.ipek@bil.omu.edu.tr>
2021-11-18 21:56:56 +0300

    add notification list

--- CoreDemo/Controllers/NotificationController.cs
+++ CoreDemo/Controllers/NotificationController.cs
@@ -3,14 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CoreDemo.Controllers
 {
     public class NotificationController : Controller
     {
+        NotificationManager notificationManager = new NotificationManager(new EfNotificationRepository());
         public IActionResult Index()
         {
             return View();
         }
+        [AllowAnonymous]
+        public IActionResult AllNotification()
+        {
+            var values = notificationManager.GetList();
+            return View(values);
+        }
     }
+
 }
